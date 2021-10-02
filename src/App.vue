ed4eb7537d6eefa7cb9014ad6aac2dbf21764ffc
hyeonsooahn <djthqtu1@naver.com>
2021-10-03 07:12:33 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div class="black-bg" v-if="모달창열렸니==true">
-    <div class="white-bg">
-      <h4>상세페이지임</h4>
-      <p>상세페이지 내용임</p>
-      <button @click="모달창열렸니=false">닫기</button>
-    </div>
-  </div>
+  <transition name="fade">
+  <Modal @closeModal="모달창열렸니 = false" :원룸들="원룸들" :누른거="누른거" :모달창열렸니 = "모달창열렸니"/>
+  </transition>
 
   <div class="menu">
     <a v-for="a in 메뉴들" :key="a">{{a}}</a>
   </div>
-  원룸샵
-  <div v-for="a in 원룸들" :key="a">
-    <img :src="a.image" class="room-img">
-    <h4>{{a.title}}</h4>
-    <p>{{a.price}}</p>
-  </div>
+
+  <Discount v-if="showDiscount=true" :discount="discount"/>
+
+  <button @click="ascSort">가격낮은순정렬</button>
+  <button @click="desSort">가격높은순정렬</button>
+  <button @click="nameSort">이름순정렬</button>
+  <button @click="sortBack">되돌리기</button>
+
+  <Card @openModal="모달창열렸니 = true; 누른거 = $event" v-for="원룸 in 원룸들" :key="원룸" :원룸="원룸"/>
+  
+
 
 </template>
 
 <script>
 
 import data from './assets/oneroom.js';
+import Discount from './Discount.vue';
+import Modal from './Modal.vue';
+import Card from './card.vue';
 
 export default {
   name: 'App',
   data(){
     return {
+      discount : 30,
+      showDiscount : true,
+      원룸들오리지널 : [...data],
+      누른거 : 0,
       원룸들 : data,
       모달창열렸니 : false,
       신고수 : [0,0,0],
@@ -39,19 +47,78 @@ export default {
     increase(){
       this.신고수++;
     },
+    sortBack(){
+      this.원룸들 = [...this.원룸들오리지널];
+    },
+    ascSort(){
+      this.원룸들.sort(function(a,b){
+        return a.price - b.price;
+      });
+    },
+    desSort(){
+      this.원룸들.sort(function(a,b){
+        return b.price - a.price;
+      });
+    },
+    nameSort(){
+      this.원룸들.sort(function(a,b){
+        return a.title<b.title ? -1 : a.title>b.title ? 1:0;
+      });
+    },
   },
+
+  mounted() {
+    setInterval(() => {
+        this.discount--;
+      }, 1000);
+  },
+
+
+  
+
   components: {
+    Discount : Discount,
+    Modal : Modal,
+    Card : Card,
   }
 }
 </script>
 
 <style>
+.fade-leave-from{
+  opacity: 1;
+}
+.fade-leave-active{
+  transition: all 1s;
+}
+.fade-leave-to{
+  opacity: 0;
+}
+
+.fade-enter-from{
+  transform: translate(-1000px);
+}
+.fade-enter-active{
+  transition: all 1s;
+}
+.fade-enter-to{
+  transform: translate(0px);
+}
+
 body{
   margin: 0;
 }
 div{
   box-sizing: border-box;
 }
+
+.discount{
+  background: #eee;
+  padding: 10%;
+  margin: 10px;
+  border-radius: 5px;
+}
+
 .black-bg{
   width: 100%; height: 100%;
   background: rgba(0, 0, 0, 0.5);
@@ -59,7 +126,7 @@ div{
   padding: 20px;
 }
 .white-bg{
-  width: 100;
+  width: 50%;
   background: white;
   border-radius: 8px;
   padding: 20px;
---
+++ src/Modal.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="black-bg" v-if="모달창열렸니==true">
+    <div class="white-bg">
+      <img :src="원룸들[누른거].image" style="width : 100%">
+      <h4>{{원룸들[누른거].title}}</h4>
+      <p>{{원룸들[누른거].content}}</p>
+      <input v-model.number="month">
+      <p> {{month}}개월 선택함 : {{원룸들[누른거].price * month}}</p>
+      <button @click="close">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Modal',
+    data(){
+      return{
+        month : 3,
+      }
+    },
+    watch : {
+      month(a){
+       if(a>=13){
+         alert("13이상 입력하지 마셈");
+         this.month = 1;
+       }else if(isNaN(a)){
+          alert("글자 쓰지 마삼");
+          this.month = 1;
+       }
+      }
+    },
+    props : {
+      원룸들 : Array,
+      누른거 : Number,
+      모달창열렸니 : Boolean,
+    },
+    methods : {
+      close(){
+        this.$emit("closeModal");
+      }
+    },
+
+    updated() {
+    if(this.month<3&&this.month!=""){
+      alert("3개월부터만 하셈");
+      this.month=3;
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
